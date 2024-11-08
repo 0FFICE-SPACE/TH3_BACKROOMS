e717cb2f8a887a59021b88a1b700f957d5093251
0FFICE_SPACE <136324965+0FFICE-SPACE@users.noreply.github.com>
2024-11-08 10:32:50 +0000

    added time changes

--- DRUMS/Funky_Groove.rb
+++ DRUMS/Funky_Groove.rb
@@ -1,12 +1,18 @@
 use_bpm 127
 
-define :d_ply_beat do
+define :d_ply_beat do  |x|
   live_loop :ply_beat do
     with_synth :sc808_snare do
     play rrand_i(45, 67), amp: rand(0.8), attack: rand(0.1), attack_level: rand(0.7), release: rand(0.3) if (spread rrand_i(1, 7), 7).tick end
     sample :hat_bdu, amp: rand(1.5), release: rand if (spread rrand_i(1, 12), 12).look
-    sample :bd_klub, amp: 2 if (spread rrand_i(1, 6), 8).look
-    sleep 0.25
+    sample :bd_klub, amp: 1.2 if (spread rrand_i(1, 6), 8).look
+    sleep x
   end
 end
-d_ply_beat
+
+
+/main/
+loop do
+  d_ply_beat [0.25,0.5].tick
+  sleep 36
+end
